c99099b10968790e1f35c3e4d5b40e7131bdef97
62115152016.3 <62115152016.3@62DE174773.senacrjedu.local>
2017-10-17 16:42:31 -0200

    17102017

---
+++ src/java/br/com/lojaWEB/model/Item.java
@@ -0,0 +1,95 @@
+
+package br.com.lojaWEB.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Item implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Item)) {
+            return false;
+        }
+        Item other = (Item) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.lojaWEB.model.Item[ id=" + id + " ]";
+    }
+    
+    private int quant;
+    private double valorItens;
+
+    @OneToOne
+    private Produto produto;
+    
+    @OneToOne
+    private Pedido pedido;
+    
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+
+    public double getValorItens() {
+        return valorItens;
+    }
+
+    public void setValorItens() {
+        this.valorItens = quant * produto.getValor();
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+    
+    
+    
+}
---
+++ src/java/br/com/lojaWEB/model/Pedido.java
@@ -0,0 +1,131 @@
+
+package br.com.lojaWEB.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Pedido implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pedido)) {
+            return false;
+        }
+        Pedido other = (Pedido) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.lojaWEB.model.Pedido[ id=" + id + " ]";
+    }
+    
+    //Atributos
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar dataPedido;
+    
+    private double valor;
+    private double frete;
+    private double total;
+    
+    @Column(nullable = false)
+    private boolean fechado;
+    
+    @Column(nullable = false)
+    private boolean pago;
+   
+    @OneToOne
+    private Cliente cliente;
+
+    //Gets e Sets
+    public Calendar getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(Calendar dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getFrete() {
+        return frete;
+    }
+
+    public void setFrete(double frete) {
+        this.frete = frete;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public boolean isFechado() {
+        return fechado;
+    }
+
+    public void setFechado(boolean fechado) {
+        this.fechado = fechado;
+    }
+
+    public boolean isPago() {
+        return pago;
+    }
+
+    public void setPago(boolean pago) {
+        this.pago = pago;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+    
+}
